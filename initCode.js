7fa3a4afe57fe9c39204fbedd01e3f0947947a6b
nxght <42642616+kmohan2606@users.noreply.github.com>
2024-11-14 19:34:27 -0500

    initial update
    test

---
+++ initCode.js
@@ -0,0 +1,12 @@
+Bangle.setOptions
+({
+  wakeOnBTN1: true,
+  wakeOnFaceUp: false,
+  wakeOnTwist: false,
+  powerSave: true,
+  lockTimeout: 10000,
+  lcdPowerTimeout: 0,
+  backlightTimeout: 10000,
+  hrmPollInterval: 40,
+});
+
